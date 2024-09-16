b7e67d134dd18394586f6c938b4a68d39432bfb0
Knut Ahlers <knut@ahlers.me>
2024-09-16 20:18:37 +0200

    Remove git-crypt dir check

--- git-wrapper.sh
+++ git-wrapper.sh
@@ -2,7 +2,7 @@
 $(dirname $0)/git.bin "$@"
 EC=$?
 
-if [ "$1" = "checkout" -a -d ".git-crypt" -a -f ".git-crypt-key" -a ! "$GIT_CRYPT_RUNNING" = "true" ]; then
+if [ "$1" = "checkout" -a -f ".git-crypt-key" -a ! "$GIT_CRYPT_RUNNING" = "true" ]; then
   export GIT_CRYPT_RUNNING=true
   export VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id="${VAULT_ROLE_ID:-}")
 
